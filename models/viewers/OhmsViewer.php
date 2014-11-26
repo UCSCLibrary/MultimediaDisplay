7f0906ae194fba3eebe0e0e3106ae6c4692acc42
NedHenry <ethenry@ucsc.edu>
2014-11-26 12:43:57 -0800

    fixed Ohms viewer so to make it actually work

--- models/viewers/OhmsViewer.php
+++ models/viewers/OhmsViewer.php
@@ -154,8 +154,8 @@ class Mmd_Ohms_Viewer extends Mmd_Abstract_Viewer
         require_once dirname(dirname(dirname(__FILE__))).'/libraries/ohmsviewer/lib/CacheFile.class.php';
         
         $plugin_dir = dirname(dirname(dirname(__FILE__)));
-        //$cacheFile = CacheFile::getInstance($cachefile,dirname(dirname($plugin_dir)).'/files',$config);
-        $cacheFile = CacheFile::getInstance($cachefile,'/var/www/html/omeka/files',$config);
+        $cacheFile = CacheFile::getInstance($cachefile,dirname(dirname($plugin_dir)).'/files',$config);
+        //$cacheFile = CacheFile::getInstance($cachefile,'/var/www/html/omeka/files',$config);
         //dirname(dirname(dirname(dirname(__FILE__)))).'/files'
 
 
